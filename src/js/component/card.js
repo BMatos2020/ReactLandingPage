4e7d0beaabae95206ec158959b9e921cd9befcc5
Brenda Matos <brenda.matosux@gmail.com>
2020-02-04 00:41:53 +0000

    react practice day 1

---
+++ src/js/component/card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function Card() {
+	return (
+		<div className="card" style={{ width: "18rem;" }}>
+			<img className="card-img-top" src="..." alt="Card image cap" />
+			<div className="card-body">
+				<p className="card-text">
+					Some quick example text to build on the card title and make
+					up the bulk of the cards content.
+				</p>
+			</div>
+		</div>
+	);
+}
---
+++ src/js/component/navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function Navbar() {
+	return (
+		<div className="text-center mt-0">
+			<nav className="navbar navbar-dark bg-dark">
+				<a className="navbar-brand" href="#">
+					Navbar
+				</a>
+			</nav>
+
+			{/* <!-- As a heading --> */}
+
+			{/* <nav className="navbar navbar-dark bg-dark">
+				<span className="navbar-brand mb-0 h1">
+					Practice with React
+				</span>
+			</nav> */}
+		</div>
+	);
+}
